68ef97945a80c4fbcdeb861a05772615cea03a1d
AgIgor <agigor@outlook.com>
2023-03-16 13:18:12 -0300

    carga eletronica com arduino

--- Amperimetro_Hall/Amperimetro_Hall.ino
+++ Amperimetro_Hall/Amperimetro_Hall.ino
@@ -10,20 +10,20 @@ float Vin = 0;
 float I = 0;
 
 
-#define kp 1.5 //0.5
-#define ki 1.2 //0.2
-#define kd 1.1 //0.1
+// #define kp 1.5 //0.5
+// #define ki 1.2 //0.2
+// #define kd 1.1 //0.1
 
-double setpoint = 0.5;   // Valor de corrente desejado (em Ampères)
+// double setpoint = 0.5;   // Valor de corrente desejado (em Ampères)
 
-double output = 0.0;     // Valor de saída do controlador (em PWM)
-double error = 0.0;      // Erro entre o setpoint e o input
-double last_error = 0.0; // Último erro calculado
-double integral = 0.0;   // Acumulador da integral do erro
-double derivative = 0.0; // Derivada do erro
+// double output = 0.0;     // Valor de saída do controlador (em PWM)
+// double error = 0.0;      // Erro entre o setpoint e o input
+// double last_error = 0.0; // Último erro calculado
+// double integral = 0.0;   // Acumulador da integral do erro
+// double derivative = 0.0; // Derivada do erro
 
-double output_min = 0.0;
-double output_max = 255.0;
+// double output_min = 0.0;
+// double output_max = 255.0;
 
 void setup() {
   Serial.begin(115200);
@@ -49,35 +49,35 @@ void loop() {
 
   media = soma/leituras;
   
-  error = setpoint - media;
-  integral = integral + error;
-  // Limitação da integral do erro
-  if (integral > output_max / ki) {
-    integral = output_max / ki;
-  }
-  else if (integral < output_min / ki) {
-    integral = output_min / ki;
-  }
+  // error = setpoint - media;
+  // integral = integral + error;
+  // // Limitação da integral do erro
+  // if (integral > output_max / ki) {
+  //   integral = output_max / ki;
+  // }
+  // else if (integral < output_min / ki) {
+  //   integral = output_min / ki;
+  // }
   
   // Cálculo da derivada do erro
-  derivative = error - last_error;
+  // derivative = error - last_error;
   
   // Cálculo da saída do controlador
-  output = kp * error + ki * integral + kd * derivative;
+  // output = kp * error + ki * integral + kd * derivative;
   
   // Limitação da saída do controlador
-  if (output > output_max) {
-    output = output_max;
-  }
-  else if (output < output_min) {
-    output = output_min;
-  }
+  // if (output > output_max) {
+  //   output = output_max;
+  // }
+  // else if (output < output_min) {
+  //   output = output_min;
+  // }
   
   // Escreve o valor da saída no pino PWM
-  analogWrite(pinPWM, output);
+  // analogWrite(pinPWM, output);
   
   // Atualiza o valor do último erro
-  last_error = error;
+  // last_error = error;
 
   // if(Iset < media){
   //   analogWrite(pinPWM, 0);
@@ -91,7 +91,7 @@ void loop() {
   // Serial.print(" error = ");
   // Serial.print(error);
 
-  Serial.print(" out = ");
-  Serial.println(output);
-  delay(10);
+  // Serial.print(" out = ");
+  // Serial.println(output);
+  delay(100);
 }
